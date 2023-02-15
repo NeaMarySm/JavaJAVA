a8788e2e4081be2b4ce496f8f8daccdaf3e7527d
NeaMary <neamarysm@gmail.com>
2023-02-15 14:51:10 +0300

    null check added

--- src/main/java/S1.java
+++ src/main/java/S1.java
@@ -27,53 +27,71 @@ public class S1 {
 
         int highestBit = Integer.highestOneBit(i);
         System.out.println("Старший бит:" + highestBit);
-        System.out.println("Номер старшего бита: " + (binary.length()-1));
+        System.out.println("Номер старшего бита: " + (binary.length() - 1));
 
         int[] m1 = S1.findMultiples(i, n);
-//        S1.printArray(m1);
+        S1.printArray(m1);
         int[] m2 = S1.findNonMultiples(i, n);
-//        System.out.println();
-//        S1.printArray(m2);
+        System.out.println();
+        S1.printArray(m2);
     }
+
     static void printArray(int[] array) {
         for (int value : array) {
             System.out.print(value + " ");
         }
     }
+
     static int[] findMultiples(int rand, int n) {
         int[] rangeShorts = new int[Short.MAX_VALUE - rand];
         int resultLength = 0;
-        for (int i = 0; i < rangeShorts.length; i++) {
-            if (rand % n == 0) resultLength++;
-            rangeShorts[i] = rand++;
-        }
-        int[] result = new int[resultLength];
-        int index = 0;
-        for (int num : rangeShorts) {
-            if (num % n == 0) {
-                result[index] = num;
-                index++;
+        if (n == 0 || n == 1) {
+            for (int i = 0; i < rangeShorts.length; i++) {
+                rangeShorts[i] = rand++;
             }
+            return rangeShorts;
+        } else {
+            for (int i = 0; i < rangeShorts.length; i++) {
+                if (rand % n == 0) resultLength++;
+                rangeShorts[i] = rand++;
+            }
+            int[] result = new int[resultLength];
+            int index = 0;
+            for (int num : rangeShorts) {
+                if (num % n == 0) {
+                    result[index] = num;
+                    index++;
+                }
+            }
+            return result;
         }
-        return result;
     }
+
     static int[] findNonMultiples(int rand, int n) {
         int[] rangeShorts = new int[-Short.MIN_VALUE + rand];
         int resultLength = 0;
-        for (int i = 0; i < rangeShorts.length; i++) {
-            if (rand % n != 0) resultLength++;
-            rangeShorts[i] = rand--;
-        }
-        int[] result = new int[resultLength];
-        int index = 0;
-        for (int num : rangeShorts) {
-            if (num % n != 0) {
-                result[index] = num;
-                index++;
+        if (n == 0 || n == 1) {
+            for (int i = 0; i < rangeShorts.length; i++) {
+                rangeShorts[i] = rand++;
             }
+            return rangeShorts;
+        } else {
+            for (int i = 0; i < rangeShorts.length; i++) {
+                if (rand % n != 0) resultLength++;
+                rangeShorts[i] = rand--;
+            }
+            int[] result = new int[resultLength];
+            int index = 0;
+            for (int num : rangeShorts) {
+                if (num % n != 0) {
+                    result[index] = num;
+                    index++;
+                }
+            }
+            return result;
         }
-        return result;
     }
+
     static int findNumberOfHighestBit(int number) {
         int count = 0;
         while (number != 1) {
